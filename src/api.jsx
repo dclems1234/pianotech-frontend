70b3669f7906064cd94f29ff9537edf172eb0628
David Clements <dclems1234@gmail.com>
2026-05-11 18:33:06 -0400

    Update API base URL to Azure

--- src/api.jsx
+++ src/api.jsx
@@ -1,7 +1,7 @@
 ﻿import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://192.168.5.231:5000/api",
+    baseURL: "https://pianotechpro.azurewebsites.net/api",
 });
 
 api.interceptors.request.use((config) => {
@@ -13,3 +13,4 @@ api.interceptors.request.use((config) => {
 });
 
 export default api;
+
